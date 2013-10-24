16add8e7ebd2d76af8f249181e35d4c56a793bb6
Richard Barella <ribabb@hotmail.com>
2013-10-24 14:50:19 -0700

    Updated Character.java
    Updated to include changes made to Object.java

--- Zed-Eclipse/src/zed/Character.java
+++ Zed-Eclipse/src/zed/Character.java
@@ -1,6 +1,8 @@
 
 package zed;
 
+import org.newdawn.slick.SpriteSheet;
+
 /*
  * @author Richard Barella Jr.
  * @author Adam Bennett
@@ -28,16 +30,16 @@ public class Character extends Object {
     
     int AI_State; // For use in Artificial_Intelligence function
     
-    public Character(int x_position, int y_position,
-            String[][] frame, int frames_count,
-            int[] frame_count, int current_frames,
-            boolean top_visible, boolean bot_visible,
+    public Character(int tile_x, int tile_y, boolean visible,
+            int sprite_shift, int tilesize, // how far sprite is shifted and size in pixels
+            SpriteSheet sprites, int[] spritesheet_index, int[] animation_length,
+            int current_animation,
             int health, float speed,
             int x_movement, int y_movement) {
         
         // Constructs the "Object" part of Character
-        super(x_position, y_position, frame, frames_count,
-                frame_count, bot_visible);
+        super(tile_x, tile_y, visible, sprite_shift, tilesize, sprites,
+                spritesheet_index, animation_length, current_animation);
         
         // Initialze health
         Health = health;
@@ -51,7 +53,7 @@ public class Character extends Object {
         // Initialize movement
         last_move = System.nanoTime();
         
-        // Initialize Artificail Intelligence
+        // Initialize Artificial Intelligence
         AI_State = 0;
     }
 
@@ -59,8 +61,12 @@ public class Character extends Object {
     public Character() {
         String[][] frame_top = new String[4][5];
         String[][] frame_bot = new String[4][5];
-        Init(0, 0, frame_bot, frame_top, false, false,
-                0, 0, 0, 0);
+        
+        Init(0, 0, false,
+            0, 0, // how far sprite is shifted and size in pixels
+            null, null, null,
+            0);
+        
         Health = 0;
         Max_Health = 0;
         Speed = 0;
@@ -72,11 +78,12 @@ public class Character extends Object {
     // are the frames that Character is using.
     
     // Initialization for 16x32 Character
-    public void Init(int x_position, int y_position,
-            String[][] frame_bot, String[][] frame_top, 
-            boolean top_visible, boolean bot_visible,
-            int health, float speed, int x_movement,
-            int y_movement){
+    public void Init(int tile_x, int tile_y, boolean visible,
+            int sprite_shift, int tilesize, // how far sprite is shifted and size in pixels
+            SpriteSheet sprites, int[] spritesheet_index, int[] animation_length,
+            int current_animation,
+            int health, float speed,
+            int x_movement, int y_movement){
         
         int frames_count = 5;
         int[] frame_count = new int[5];
@@ -86,10 +93,10 @@ public class Character extends Object {
         frame_count[3] = 4;
         frame_count[4] = 4;
         
-        Init(x_position, y_position,
-            frame_bot, frame_top,
-            frames_count, frame_count,
-            top_visible, bot_visible);
+        Init(tile_x, tile_y, visible,
+            sprite_shift, tilesize, // how far sprite is shifted and size in pixels
+            sprites, spritesheet_index, animation_length,
+            current_animation);
         
         Health = health;
         Speed = speed;
@@ -98,7 +105,7 @@ public class Character extends Object {
         
         last_move = System.nanoTime();
     }
-    
+    /*
     // Initialization for 16x16 Character
     public void Init(int x_position, int y_position,
             String[][] frame_bot,
@@ -127,6 +134,7 @@ public class Character extends Object {
         
         last_move = System.nanoTime();
     }
+*/
     
     // Updates the Character
     public void Update(Object collision_objects[]){
@@ -153,30 +161,38 @@ public class Character extends Object {
     // updates the current animation being played based on current movement
     void Update_Frame_State(){
         
-        if (Y_Movement == 1)
-            Frame_State = FRAME_STATE_DOWN_WALK;
+        if (Y_Movement == 1) {
+            Current_Animation = Animation_List[FRAME_STATE_DOWN_WALK];
+        }
         
-        else if (Y_Movement == -1)
-            Frame_State = FRAME_STATE_UP_WALK;
+        else if (Y_Movement == -1) {
+            Current_Animation = Animation_List[FRAME_STATE_UP_WALK];
+        }
         
         else
         {
-            if (X_Movement == 1)
-                Frame_State = FRAME_STATE_RIGHT_WALK;
+            if (X_Movement == 1) {
+                Current_Animation = Animation_List[FRAME_STATE_RIGHT_WALK];
+            }
             
-            else if (Y_Movement == -1)
-                Frame_State = FRAME_STATE_LEFT_WALK;
+            else if (Y_Movement == -1) {
+                Current_Animation = Animation_List[FRAME_STATE_LEFT_WALK];
+            }
             
             else
             {
-                if (Frame_State == FRAME_STATE_UP_WALK)
-                    Frame_State = FRAME_STATE_UP;
-                else if (Frame_State == FRAME_STATE_LEFT_WALK)
-                    Frame_State = FRAME_STATE_LEFT;
-                else if (Frame_State == FRAME_STATE_DOWN_WALK)
-                    Frame_State = FRAME_STATE_DOWN;
-                else if (Frame_State == FRAME_STATE_RIGHT_WALK)
-                    Frame_State = FRAME_STATE_RIGHT;
+                if (Current_Animation == Animation_List[FRAME_STATE_UP_WALK]) {
+                    Current_Animation = Animation_List[FRAME_STATE_UP];
+                }
+                else if (Current_Animation == Animation_List[FRAME_STATE_LEFT_WALK]) {
+                    Current_Animation = Animation_List[FRAME_STATE_LEFT];
+                }
+                else if (Current_Animation == Animation_List[FRAME_STATE_DOWN_WALK]) {
+                    Current_Animation = Animation_List[FRAME_STATE_DOWN];
+                }
+                else if (Current_Animation == Animation_List[FRAME_STATE_RIGHT_WALK]) {
+                    Current_Animation = Animation_List[FRAME_STATE_RIGHT];
+                }
             }
         }
     }
