5629d38bc82ca69e2cf130153cab2d02fa386bb1
ribab <ribab127@gmail.com>
2013-11-07 13:39:57 -0800

    Forgot to check new files

---
+++ Zed-Eclipse/src/zed/GPortal.java
@@ -0,0 +1,98 @@
+/**
+ * 
+ */
+package zed;
+
+import org.newdawn.slick.Animation;
+import org.newdawn.slick.SpriteSheet;
+
+/**
+ * @author Richard Barella Jr.
+ * @author Adam Bennett
+ * @author Ryan Slyter
+ */
+public class GPortal extends GObject {
+
+	private int Dest_Level; // number that represents the level to load
+	                        // if this is the same as the current level,
+	                        // level shouldn't re-load but should only update
+	                        // the player's position.
+	private int Dest_X; // x position of destination tile
+	private int Dest_Y; // y position of destination tile
+	
+	/**
+	 * Default constructor for GPortal
+	 */
+	public GPortal() {
+		super();
+		Dest_Level = 0;
+		Dest_X = 0;
+		Dest_Y = 0;
+	}
+	
+	/**
+	 * Constructor for GPortal
+	 */
+	public GPortal(int tile_x, int tile_y, int tilesize,
+			int dest_level, int dest_x, int dest_y) {
+		
+		// Initialize animation to default
+		Animation[] no_animation = new Animation[1];
+		no_animation[1] = new Animation();
+		
+		// Construct the GObject part
+		super.Init(tile_x, tile_y, false, false, null, null, tilesize, no_animation, 0);
+		
+		Dest_Level = dest_level;
+		Dest_X = dest_x;
+		Dest_Y = dest_y;
+	}
+	
+	// Returns the number that represents the level that the portal ports to
+	public int Get_Dest_Level(){
+		
+		return Dest_Level;
+	}
+	
+	// Returns the x position of the tile that player should port to
+	public int Get_Dest_X_Tile(){
+		
+		return Dest_X;
+	}
+	
+	// Returns the x position of the pixel that player should port to
+	public int Get_Dest_X_Pixel(){
+		
+		return Dest_X*16;
+	}
+	
+	// Returns the y position of the tile that player should port to
+	public int Get_Dest_Y_Tile(){
+		
+		return Dest_Y;
+	}
+	
+	// Returns the x position of the pixel that player should port to
+	public int Get_Dest_Y_Pixel(){
+		
+		return Dest_Y*16;
+	}
+	
+	// Changes the destination level of the portal
+	public void Set_Dest_Level(int new_dest_level){
+		
+		Dest_Level = new_dest_level;
+	}
+	
+	// Changes the x destination of where player ports to
+	public void Set_Dest_X_Tile(int new_dest_x){
+		
+		Dest_X = new_dest_x;
+	}
+	
+	// CHange sthe y destination of where player ports to
+	public void Set_Dest_Y_Tile(int new_dest_y){
+		
+		Dest_Y = new_dest_y;
+	}
+}
---
+++ Zed-Eclipse/src/zed/Zombie.java
@@ -0,0 +1,76 @@
+package zed;
+
+import org.newdawn.slick.Animation;
+import org.newdawn.slick.SpriteSheet;
+
+public class Zombie extends GCharacter {
+
+	public Zombie(){
+		
+		
+	}
+	// Zombie constructor
+	public Zombie(int tile_x, int tile_y, SpriteSheet sprites) {
+
+		Animation[] zombie_animations = new Animation[4];
+		
+		int i0 = 10;
+		int[] spritesheet_index = {i0, i0 + 1, i0 + 2, i0 + 3};
+		int[] animation_length = {3, 3, 3, 3};
+		boolean[] looping = {true, true, true, true};
+		int[] sprite_shift_x = {0, 0, 0, 0};
+		int[] sprite_shift_y = {16, 16, 16, 16};
+		
+        for (int i = 0; i < 4; i++) // initialize walking and standing animations
+        {
+	        zombie_animations[i] = new Animation(sprites, // spritesheet to use
+	        		// location of first sprite in spritesheet
+	                0,                       spritesheet_index[i],
+	                // location of last sprite in spritesheet
+	                animation_length[i] - 1, spritesheet_index[i],
+	                false, // horizontalScan value
+	                200, // speed value for animation
+	                true // autoupdate value
+	                );
+	        zombie_animations[i].setLooping(true); // intialize whether animation loops
+	        zombie_animations[i].setPingPong(true); // initialize whether animation ping-pongs between last and first frame
+        }
+        
+        FRAME_STATE_UP = 3; FRAME_STATE_DOWN = 0;
+        FRAME_STATE_LEFT = 1; FRAME_STATE_RIGHT = 2;
+        FRAME_STATE_UP_WALK = 3; FRAME_STATE_DOWN_WALK = 0;
+        FRAME_STATE_LEFT_WALK = 1; FRAME_STATE_RIGHT_WALK = 2;
+		
+        // Initialize zombie based on constructor
+		super.Init(tile_x, tile_y, true, false,
+				sprite_shift_x, sprite_shift_y, 16, zombie_animations,
+				0, 5, 1.0f, 0, 0);
+		
+		AI_State_Change_Time = 500;
+	}
+	
+	public void Artificial_Intelligence(boolean collision, Player_Character player)
+	{
+		int x_distance = player.Get_X_Position() - X_Position;
+		int y_distance = player.Get_Y_Position() - Y_Position;
+		if (x_distance*x_distance + y_distance*y_distance > 16*5*16*5)
+		{
+			super.Artificial_Intelligence(collision, player);
+		}
+		else
+		{
+			if (x_distance*x_distance > y_distance*y_distance)
+			{
+				if (x_distance > 0) {X_Movement = 1;}
+				else                {X_Movement = -1;}
+				Y_Movement = 0;
+			}
+			else // y_distance >= x_distance
+			{
+				if (y_distance > 0) {Y_Movement = 1;}
+				else                {Y_Movement = -1;}
+				X_Movement = 0;
+			}
+		}
+	}
+}
