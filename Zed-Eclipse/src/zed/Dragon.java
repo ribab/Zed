cf8511755c2235317780bb322a70211802b44243
ribab <ribab127@gmail.com>
2013-12-03 17:46:21 -0800

    Made dragon spit fire and added Ryan's gameover screen

--- Zed-Eclipse/src/zed/Dragon.java
+++ Zed-Eclipse/src/zed/Dragon.java
@@ -12,7 +12,9 @@ public class Dragon extends GCharacter {
 	static int Type = 100;
 	
 	private static final int NUMBER_OF_FIREBALLS = 3;
+	private static final int FIREBALL_FREQUENCY_MILLI = 3000;
 	private GCharacter[] fireballs;
+	private long fireballtimer;
 	
 	// Zombie default constructor does nothing
 	public Dragon() throws SlickException{
@@ -62,7 +64,7 @@ public class Dragon extends GCharacter {
         FRAME_STATE_UP_WALK = 3; FRAME_STATE_DOWN_WALK = 0;
         FRAME_STATE_LEFT_WALK = 1; FRAME_STATE_RIGHT_WALK = 2;
 		
-        // Initialize zombie based on constructor
+        // Initialize dragon based on constructor
 		super.Init(
 				tile_x, tile_y, // Tile to put in
 				24, 24,
@@ -81,51 +83,84 @@ public class Dragon extends GCharacter {
 		// Construct fireballs
 		fireballs = new GCharacter[NUMBER_OF_FIREBALLS];
 		int[] fireball_sprite_shift = {0};
-		int[] fireball_spritesheet_index = {0}; // TODO: change later
-		int[] fireball_animation_length = {1};
+		int[] fireball_spritesheet_index = {8}; // TODO: change later
+		int[] fireball_animation_length = {4};
 		boolean[] fireball_looping = {true};
 		for (int i = 0; i < NUMBER_OF_FIREBALLS; i++)
 		{
+			SpriteSheet fireballsprites = new SpriteSheet(sprites.getSubImage(0, 0, sprites.getWidth(), sprites.getHeight()), 16, 16);
 			fireballs[i] = new GCharacter(
 					-100, // initial x position of the character w.r.t. the game tiles
 		    		-100, // initial y position of the character w.r.t. the game tiles
 		    		16, // initial width in pixels
 		    		16, // initial height in pixels
-		    		true, // initialize whether the character is visible
-		    		true, // initializes whether its solid
+		    		false, // initialize whether the character is visible
+		    		false, // initializes whether its solid
 		    		1, // initialize damage to the player
 		            fireball_sprite_shift, // by how many pixels each animation is shifted in x direction
 		            fireball_sprite_shift, // by how many pixels each animation is shifted in y direction
 		            16, // Give the character how large each tile is
-		            sprites, // Give the character the spritesheet file for fetching its animation frames
+		            fireballsprites, // Give the character the spritesheet file for fetching its animation frames
 		            fireball_spritesheet_index, // Give the character the indexes for the rows of the SpriteSheet to fetch each animation from
 		                                     // index are defined {up,left,down,right,upwalk,leftwalk,downwalk,rightwalk}
 		            fireball_animation_length, // Give the character the length of each animation
 		            fireball_looping, // tell which animations are looping
 		            0, // intialize which animation to start out with
 		            1, // intialize the character's max_health and health
-		            4.0f, // Give the character its speed in tiles per second 
+		            8.0f, // Give the character its speed in tiles per second 
 		            0, // Give the character its initial x_movement value (-1, 0, 1)
 		            0, // Give the character its initial y_movement value (-1, 0, 1)
 		            10
 		            );
+			fireballs[i].Animation_List[0].setPingPong(false);
+			fireballs[i].Health = 0;
 		}
+		
+		fireballtimer = System.currentTimeMillis();
 	}
 	
 	public void Spit_Fireball()
 	{
 		boolean spit = false;
-		for (int i = 0; i < fireballs.length && !spit; i++)
+		if (System.currentTimeMillis() > fireballtimer + FIREBALL_FREQUENCY_MILLI)
 		{
-			if (fireballs[i].Health > 0)
+			for (int i = 0; i < fireballs.length && !spit; i++)
 			{
-				spit = true;
-				fireballs[i].X_Position = X_Position;
-				fireballs[i].Y_Position = Y_Position;
-				fireballs[i].X_Movement = X_Movement;
-				fireballs[i].Y_Movement = Y_Movement;
-				fireballs[i].Increase_Health(1);
-				fireballs[i].Visible = true;
+				if (fireballs[i].Health <= 0
+						&& !fireballs[i].Visible
+						&& !fireballs[i].Solid)
+				{
+					spit = true;
+					fireballtimer = System.currentTimeMillis();
+					fireballs[i].X_Position = X_Position + 4;
+					fireballs[i].Y_Position = Y_Position + 4;
+					if (Current_Animation_Index == FRAME_STATE_UP
+							|| Current_Animation_Index == FRAME_STATE_UP_WALK)
+					{
+						fireballs[i].X_Movement = 0;
+						fireballs[i].Y_Movement = -1;
+					}
+					else if (Current_Animation_Index == FRAME_STATE_LEFT
+							|| Current_Animation_Index == FRAME_STATE_LEFT_WALK)
+					{
+						fireballs[i].X_Movement = -1;
+						fireballs[i].Y_Movement = 0;
+					}
+					else if (Current_Animation_Index == FRAME_STATE_DOWN
+							|| Current_Animation_Index == FRAME_STATE_DOWN_WALK)
+					{
+						fireballs[i].X_Movement = 0;
+						fireballs[i].Y_Movement = 1;
+					}
+					else
+					{
+						fireballs[i].X_Movement = 1;
+						fireballs[i].Y_Movement = 0;
+					}
+					fireballs[i].Health = 1;
+					fireballs[i].Visible = true;
+					fireballs[i].Solid = true;
+				}
 			}
 		}
 	}
@@ -136,13 +171,13 @@ public class Dragon extends GCharacter {
 	{
 		int x_distance = player.Get_X_Position() - X_Position;
 		int y_distance = player.Get_Y_Position() - Y_Position;
-		// Froam around randomly If zombie cannot see the player
+		// Froam around randomly If dragon cannot see the player
 		if (x_distance*x_distance + y_distance*y_distance > 16*5*16*5
 				|| !player.Visible)
 		{
 			super.Artificial_Intelligence(collision, player);
 		}
-		else // Follow the player if zombie can see the player
+		else // Follow the player if dragon can see the player
 		{
 			if (x_distance*x_distance > y_distance*y_distance)
 			{
@@ -157,58 +192,41 @@ public class Dragon extends GCharacter {
 				X_Movement = 0;
 			}
 		}
-		
-		if (collision)
-		{
-			
-		}
+		Spit_Fireball();
 	}
 	
-	public GObject X_Collision(GObject[] otherobj){
+	public void Update(GObject[] collision_objects, 
+			GCharacter[] npcs, Player_Character player) 
+	{
+		super.Update(collision_objects, npcs, player);
 		
-		GObject return_value = super.X_Collision(otherobj);
-		GObject fireballcol;
-		for (int i = 0; i < fireballs.length; i++)
-		{
-			if ((fireballcol = fireballs[i].X_Collision(otherobj)) != null)
-			{
-				if (fireballcol.Get_Type() != -1)
-					fireballs[i].Decriment_Health();
-			}
-		}
-		return return_value;
-	}
-	
-	public GObject Y_Collision(GObject[] otherobj){
-		
-		GObject return_value = super.Y_Collision(otherobj);
-		GObject fireballcol;
-		for (int i = 0; i < fireballs.length; i++)
-		{
-			if ((fireballcol = fireballs[i].Y_Collision(otherobj)) != null)
-			{
-				if (fireballcol.Get_Type() != -1)
-					fireballs[i].Decriment_Health();
-			}
-		}
-		return return_value;
-	}
-	
-	public void Update_X_Position(){
-		
-		super.Update_X_Position();
 		for (int i = 0; i < fireballs.length; i++)
 		{
 			fireballs[i].Update_X_Position();
+			fireballs[i].Update_Y_Position();
 		}
-	}
-	
-	public void Update_Y_Position(){
-		
-		super.Update_Y_Position();
+
 		for (int i = 0; i < fireballs.length; i++)
 		{
-			fireballs[i].Update_Y_Position();
+			if (fireballs[i].X_Collision(collision_objects) != null
+					|| fireballs[i].X_Out_Of_Bounds())
+			{
+				fireballs[i].Decriment_Health();
+				fireballs[i].Visible = false;
+				fireballs[i].Solid = false;
+			}
+			else if (fireballs[i].Y_Collision(collision_objects) != null
+					|| fireballs[i].Y_Out_Of_Bounds())
+			{
+				fireballs[i].Decriment_Health();
+				fireballs[i].Visible = false;
+				fireballs[i].Solid = false;
+			}
+			else if (fireballs[i].X_Collision(player)
+					|| fireballs[i].Y_Collision(player))
+			{
+				player.Decriment_Health();
+			}
 		}
 	}
 	
---
+++ Zed-Eclipse/src/zed/GameOver.java
@@ -0,0 +1,61 @@
+package zed;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import org.lwjgl.input.Mouse;
+import org.newdawn.slick.*;
+import org.newdawn.slick.state.*;
+
+public class GameOver extends BasicGameState{
+	
+	
+	Image over_background;
+	Music music;
+	
+	public GameOver(){}
+	
+	public GameOver(int state){}
+
+	@Override
+	public void init(GameContainer gc, StateBasedGame sbg)
+			throws SlickException {
+		
+		over_background = new Image("images/gameoverlow.png");
+
+        music = new Music("soundtrack/kawfy/braintwoquart.wav");
+        music.loop();
+	}
+
+	@Override
+	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
+			throws SlickException {
+		over_background.draw(0,0, 640, 480);
+		g.setColor(Color.blue);
+		g.scale(1.0f, 1.0f);
+		g.drawString("YOU HAVE DIED: PRESS SPACEBAR TO RETURN TO MENU", 115, 350);
+		
+		Zed temp = new Zed();
+		g.drawString("YOUR SCORE: ", 25, 380);
+		g.drawString("" + temp.test.objectives.percentageCompleted(), 25, 393);
+		
+	}
+
+	@Override
+	public void update(GameContainer gc, StateBasedGame sbg, int g)
+			throws SlickException {
+		Input input = gc.getInput();
+		
+		if (input.isKeyDown(Input.KEY_SPACE)){
+				
+				sbg.getState(1).init(gc, sbg);
+				sbg.enterState(0);
+		}
+	}
+
+	@Override
+	public int getID() {
+		return 2;
+	}
+
+}
